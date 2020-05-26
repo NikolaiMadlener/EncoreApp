85455a115f9aed9e22e6f09964aaf753a5569253
Nikolai Madlener <nikolai.madlener@tum.de>
2020-05-26 21:16:26 +0200

    4 commit

---
+++ encoreApp/encoreApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  encoreApp
+//
+//  Created by Nikolai Madlener on 26.05.20.
+//  Copyright © 2020 NikolaiEtienne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
