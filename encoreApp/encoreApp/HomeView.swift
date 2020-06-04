f1367a6207015b46ecac73f3a44da27c3dd864a6
NikolaiMadlener <nikolai.madlener@outlook.de>
2020-06-04 13:25:04 +0200

    change layering (songTitle_layer now in the same layer as the List below); add songTitleBar_layer above; now no wibbeling anymore; songTitleBar height now depends on device height;

--- encoreApp/encoreApp/HomeView.swift
+++ encoreApp/encoreApp/HomeView.swift
@@ -21,31 +21,31 @@ struct HomeView: View {
     var body: some View {
         ZStack {
             //Layer 0: Background Layer
-//            LinearGradient(gradient: Gradient(colors: [
-//                Color.white,
-//                Color.white,
-//                Color.white
-//            ]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            //            LinearGradient(gradient: Gradient(colors: [
+            //                Color.white,
+            //                Color.white,
+            //                Color.white
+            //            ]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
             //Layer 1: Song Queue Layer
             songQueue_layer
             
             //Layer 2: Song Title Layer
-            songTitle_layer
+            songTitleBar_layer
             
             //Layer 3: Menu Layer
             menu_layer
             
             //Layer4: Observer Layer (Debugging)
-//            VStack {
-//                Text("\(current_title_offset)")
-//                    .foregroundColor(Color.yellow)
-//                Spacer()
-//            }
+            //            VStack {
+            //                Text("\(current_title_offset)")
+            //                    .foregroundColor(Color.yellow)
+            //                Spacer()
+            //            }
         }
     }
     
-    //Layer 1: Song Queue Layer
+    //MARK: Layer 1: Song Queue Layer
     private var songQueue_layer: some View {
         ScrollView {
             GeometryReader { geo -> AnyView? in
@@ -58,10 +58,8 @@ struct HomeView: View {
                 return nil
             }
             VStack {
-                HStack {
-                    Spacer()    //Spacer for Album Cover Layer
-                        .frame(height: 320)
-                }
+                songTitle
+                
                 VStack {
                     ForEach(model.queue, id: \.self) { song in
                         VStack {
@@ -72,11 +70,89 @@ struct HomeView: View {
                         }
                     }
                 }.animation(.easeInOut(duration: 0.30))
+                    .offset(CGSize(width: 0, height: 300))
             }.background(Color.clear)
         }
     }
     
-    //Layer 2: Menu Layer
+    private var songTitle: some View {
+        GeometryReader { geo in
+            VStack {
+                HStack {
+                    Spacer()
+                    if (self.current_title_offset > -260) {
+                        VStack {
+                            Image("album1")
+                                .resizable()
+                                .frame(width: 180, height: 180)
+                            Text("\(self.model.getSongPlaying().name)")
+                                .font(.system(size: 25, weight: .bold))
+                            Text("\(self.model.getSongPlaying().artists[0])")
+                                .font(.system(size: 20, weight: .semibold))
+                        }
+                        Spacer()
+                    }
+                }
+                
+                // later this will be the playbar
+                HStack(spacing: 0) {
+                    Rectangle()
+                        .frame(width: geo.size.width * 0.5, height: 3)
+                        .foregroundColor(Color("purpleblue"))
+                    Rectangle()
+                        .frame(width: geo.size.width * 0.3, height: 3)
+                        .foregroundColor(self.colorScheme == .dark ? Color("darkgray") : Color.gray)
+                }.padding()
+                
+                Spacer()
+            }
+        }
+    }
+    
+    //MARK: Layer 2: Song Title Bar Layer
+    private var songTitleBar_layer: some View {
+        GeometryReader { geo in
+            if (self.current_title_offset <= -260) {
+                VStack(alignment: .leading) {
+                    ZStack(alignment: .top) {
+                        VStack(spacing: 0) {
+                            Rectangle()
+                                .frame(width: geo.size.width, height: geo.size.height * 0.13)
+                                .foregroundColor(Color.clear)
+                                .background(Blur(colorScheme: self.colorScheme))
+                            
+                            // later this will be the playbar
+                            HStack(spacing: 0) {
+                                Rectangle()
+                                    .frame(width: geo.size.width * (2/3), height: 3)
+                                    .foregroundColor(Color("purpleblue"))
+                                Rectangle()
+                                    .frame(width: geo.size.width * (1/3), height: 3)
+                                    .foregroundColor(self.colorScheme == .dark ? Color("darkgray") : Color.gray)
+                            }
+                            
+                        }.edgesIgnoringSafeArea(.top)
+                        HStack {
+                            Image("album1")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                            VStack(alignment: .leading) {
+                                Text("\(self.model.getSongPlaying().name)")
+                                    .font(.system(size: 20, weight: .bold))
+                                Text("\(self.model.getSongPlaying().artists[0])")
+                                    .font(.system(size: 15, weight: .semibold))
+                            }
+                            Spacer()
+                        }.padding()
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    //MARK: Layer 3: Menu Layer
     private var menu_layer: some View {
         VStack {
             HStack {
@@ -91,55 +167,6 @@ struct HomeView: View {
                 }
             }
             Spacer()
-        }
-    }
-    
-    //Layer 3: Song Title Layer
-    private var songTitle_layer: some View {
-        GeometryReader { geo in
-            VStack {
-                Spacer()
-                    .frame(height: self.current_title_offset)
-                
-                HStack {
-                    if (self.current_title_offset > -260) {
-                        VStack {
-                            Image("album1")
-                                .resizable()
-                                .frame(width: 180, height: 180)
-                            Text("\(self.model.getSongPlaying().name)")
-                                .font(.system(size: 25, weight: .bold))
-                            Text("\(self.model.getSongPlaying().artists[0])")
-                                .font(.system(size: 20, weight: .semibold))
-                        }
-                    } else {
-                        VStack(alignment: .leading) {
-                            Spacer().frame(height: 260)
-                            ZStack(alignment: .top) {
-                                Rectangle()
-                                    .frame(width: geo.size.width, height: 120)
-                                    .foregroundColor(Color.clear)
-                                    .background(Blur(colorScheme: self.colorScheme))
-                                    .edgesIgnoringSafeArea(.top)
-                                HStack {
-                                    Image("album1")
-                                        .resizable()
-                                        .frame(width: 40, height: 40)
-                                    VStack(alignment: .leading) {
-                                        Text("\(self.model.getSongPlaying().name)")
-                                            .font(.system(size: 20, weight: .bold))
-                                        Text("\(self.model.getSongPlaying().artists[0])")
-                                            .font(.system(size: 15, weight: .semibold))
-                                    }
-                                    Spacer()
-                                    }.padding()
-                            }
-                            Spacer()
-                        }
-                    }
-                }
-                Spacer()
-            }
         }
     }
 }
