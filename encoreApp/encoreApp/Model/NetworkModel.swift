3bdc29ca7a9b5a8e03b8bfe367b396bbf7cda416
etiennekoehler <etienne.koehler@gmail.com>
2020-06-28 23:53:23 +0200

    clean code in NetworkModel

--- encoreApp/encoreApp/Model/NetworkModel.swift
+++ encoreApp/encoreApp/Model/NetworkModel.swift
@@ -19,7 +19,6 @@ public class NetworkModel: ObservableObject {
     
     
     func getAuthToken() {
-        print("AuthStart")
         guard let url = URL(string: "https://api.encore-fm.com/users/"+"\(userVM.username)"+"/authToken") else {
             print("Invalid URL")
             return
@@ -27,19 +26,10 @@ public class NetworkModel: ObservableObject {
         }
         
         var request = URLRequest(url: url)
-        
         request.httpMethod = "GET"
         request.addValue(userVM.secret, forHTTPHeaderField: "Authorization")
         request.addValue(userVM.sessionID, forHTTPHeaderField: "Session")
-        print("AuthSecret:\(userVM.secret)")
-        print("SessionID:\(userVM.sessionID)")
-        print("Username:\(userVM.username)")
         
-        // HTTP Request Parameters which will be sent in HTTP Request Body
-        //let postString = "userId=300&title=My urgent task&completed=false";
-        // Set HTTP Request Body
-        //request.httpBody = postString.data(using: String.Encoding.utf8);
-        // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             // Check for Error
             if let error = error {
@@ -50,10 +40,8 @@ public class NetworkModel: ObservableObject {
             // Convert HTTP Response Data to a String
             if let data = data, let dataString = String(data: data, encoding: .utf8) {
                 print("Response data string authToken:\n \(dataString)")
-                
                 do {
                     if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                        // try to read out a string array
                         self.authToken = json["access_token"] as! String
                     }
                 } catch {
@@ -62,8 +50,6 @@ public class NetworkModel: ObservableObject {
             }
         }
         task.resume()
-        print("Got Token")
-        print("AuthEnd")
     }
     
     func getClientToken() {
@@ -74,19 +60,10 @@ public class NetworkModel: ObservableObject {
         }
         
         var request = URLRequest(url: url)
-        
         request.httpMethod = "GET"
         request.addValue(userVM.secret, forHTTPHeaderField: "Authorization")
         request.addValue(userVM.sessionID, forHTTPHeaderField: "Session")
-        print("AuthSecret:\(userVM.secret)")
-        print("SessionID:\(userVM.sessionID)")
-        print("Username:\(userVM.username)")
         
-        // HTTP Request Parameters which will be sent in HTTP Request Body
-        //let postString = "userId=300&title=My urgent task&completed=false";
-        // Set HTTP Request Body
-        //request.httpBody = postString.data(using: String.Encoding.utf8);
-        // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             // Check for Error
             if let error = error {
@@ -97,10 +74,8 @@ public class NetworkModel: ObservableObject {
             // Convert HTTP Response Data to a String
             if let data = data, let dataString = String(data: data, encoding: .utf8) {
                 print("Response data string clientToken:\n \(dataString)")
-                
                 do {
                     if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                        // try to read out a string array
                         self.clientToken = json["access_token"] as! String
                     }
                 } catch {
@@ -109,17 +84,12 @@ public class NetworkModel: ObservableObject {
             }
         }
         task.resume()
-        print("Got Token")
-        print("AuthEnd")
     }
     
     func searchSong(query: String) {
-        print("SearchStart")
-        var songs: [String] = []
         guard var components = URLComponents(string: "https://api.spotify.com/v1/search") else {
             print("Invalid URL")
             return
-            
         }
         
         components.queryItems = [
@@ -127,15 +97,9 @@ public class NetworkModel: ObservableObject {
             URLQueryItem(name: "type", value: "track")
         ]
         var request = URLRequest(url: components.url!)
-        
         request.httpMethod = "GET"
         request.addValue("Bearer "+clientToken, forHTTPHeaderField: "Authorization")
         
-        // HTTP Request Parameters which will be sent in HTTP Request Body
-        //let postString = "userId=300&title=My urgent task&completed=false";
-        // Set HTTP Request Body
-        //request.httpBody = postString.data(using: String.Encoding.utf8);
-        // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             // Check for Error
             if let error = error {
@@ -143,21 +107,13 @@ public class NetworkModel: ObservableObject {
                 return
             }
             
-            //var items: [Item] = []
-            
             // Convert HTTP Response Data to a String
             if let data = data, let dataString = String(data: data, encoding: .utf8) {
                 print("Response data string search:\n \(dataString)")
-                
                 do {
                     let decodedData = try JSONDecoder().decode(SpotifySearchPayload.self, from: data)
-                    //print("Tracksdec:\(decodedData.tracks)")
-                    
                     DispatchQueue.main.async {
                         self.items = decodedData.tracks.items
-                        for item in self.items {
-                            print(item.name)
-                        }
                     }
                 } catch {
                     print("Error Decode")
@@ -174,18 +130,11 @@ public class NetworkModel: ObservableObject {
             
         }
         var request = URLRequest(url: url)
-        
         request.httpMethod = "POST"
         request.addValue(userVM.secret, forHTTPHeaderField: "Authorization")
         request.addValue(userVM.sessionID, forHTTPHeaderField: "Session")
         
-        // HTTP Request Parameters which will be sent in HTTP Request Body
-        //let postString = "userId=300&title=My urgent task&completed=false";
-        // Set HTTP Request Body
-        //request.httpBody = postString.data(using: String.Encoding.utf8);
-        // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
             // Check for Error
             if let error = error {
                 print("Error took place \(error)")
@@ -195,13 +144,9 @@ public class NetworkModel: ObservableObject {
             // Convert HTTP Response Data to a String
             if let data = data, let dataString = String(data: data, encoding: .utf8) {
                 print("Response data string:\n \(dataString)")
-                
                 do {
                     let decodedData = try JSONDecoder().decode(Song.self, from: data)
-                    DispatchQueue.main.async {
-                        print("Successfully post of suggest song: \(decodedData.name)")
-                        
-                    }
+                    print("Successfully post of suggest song: \(decodedData.name)")
                 } catch {
                     print("Error")
                 }
--- encoreApp/encoreApp/Views/EntryViews/ContentView.swift
+++ encoreApp/encoreApp/Views/EntryViews/ContentView.swift
@@ -132,12 +132,6 @@ struct ContentView: View {
         }
     }
     
-//    func createSessionWithAccess() {
-//        let dispatchGroup = DispatchGroup()
-//        dispatchGroup.enter()
-//
-//    }
-    
     func checkUsernameInvalid(username: String) -> Bool {
         let range = NSRange(location: 0, length: username.utf16.count)
         let regex = try! NSRegularExpression(pattern: "[A-Za-z][A-Za-z][A-Za-z][A-Za-z]*")
@@ -164,15 +158,10 @@ struct ContentView: View {
             return
             
         }
-        var request = URLRequest(url: url)
         
+        var request = URLRequest(url: url)
         request.httpMethod = "POST"
         
-        // HTTP Request Parameters which will be sent in HTTP Request Body
-        //let postString = "userId=300&title=My urgent task&completed=false";
-        // Set HTTP Request Body
-        //request.httpBody = postString.data(using: String.Encoding.utf8);
-        // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             
             // Check for Error
@@ -180,7 +169,6 @@ struct ContentView: View {
                 print("Error took place \(error)")
                 return
             }
-            
             
             // Convert HTTP Response Data to a String
             if let data = data, let dataString = String(data: data, encoding: .utf8) {
