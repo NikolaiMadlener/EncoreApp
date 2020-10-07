8251166b27167750e6e2ee859512b6993c8bdc5f
etiennekoehler <etienne.koehler@gmail.com>
2020-10-07 23:42:05 +0200

    fix double divider in MenuView and refactor MenuView

--- encoreApp/encoreApp/Views/SessionViews/MenuView.swift
+++ encoreApp/encoreApp/Views/SessionViews/MenuView.swift
@@ -31,124 +31,136 @@ struct MenuView: View {
     
     var body: some View {
         GeometryReader { geo in
-            ZStack {
-                VStack(spacing: 0) {
-                    self.topBar.padding()
-                    
-                    Button(action: {
-                        withAnimation {
-                            self.showPopupQRCode.toggle()
-                        }
-                    }) {
-                        QRCodeView(url: "encoreApp://\(self.userVM.sessionID)", size: 150).padding(10)
-                    }.buttonStyle(PlainButtonStyle())
-                    
-                    Text("Let your friends scan the QR code \nor share the Session-Link to let them join. ").font(.footnote).multilineTextAlignment(.center).padding(.bottom)
-                    Button(action: { self.showShareSheet.toggle() }) {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15).frame(maxWidth: .infinity, maxHeight: 50).foregroundColor(self.colorScheme == .dark ? Color("darkgray") : Color("lightgray"))
-                            HStack {
-                                Text("Share Session-Link")
-                                    .foregroundColor(self.colorScheme == .dark ? Color.white : Color.black)
-                                    .font(.system(size: 15))
-                                    .padding(.leading)
-                                Spacer()
-                                Image(systemName: "square.and.arrow.up")
-                                    .foregroundColor(self.colorScheme == .dark ? Color.white : Color.black)
-                                    .font(.system(size: 20))
-                                    .padding(.trailing)
-                            }
-                        }.padding(.horizontal, 25)
-                    }
-                    VStack() {
-                        List {
-                            VStack {
-                                Spacer().frame(height: 10)
-                                ForEach(self.userListVM.members, id: \.self) { member in
-                                    VStack {
-                                        HStack {
-                                            if member.is_admin {
-                                                Text("\(member.username)").bold()
-                                            } else {
-                                                Text("\(member.username)")
-                                            }
-                                            Spacer()
-                                            if member.is_admin {
-                                                Image(systemName: "music.house")
-                                            }
-                                            else if member.username == self.userVM.username {
-                                                Text("You").bold()
-                                            }
-                                        }
-                                        Divider()
-                                    }
-                                }
-                                Spacer().frame(height: 10)
-                            }
-                            
-                        }.padding(.horizontal, 30)
-                            .onAppear{ UITableView.appearance().separatorColor = .clear }
-                    }
-                    Spacer().frame(height: 65)
-                }.alert(isPresented: self.$showSessionExpiredAlert) {
-                    Alert(title: Text("Session expired"),
-                          message: Text("The Host has ended the Session."),
-                          dismissButton: .destructive(Text("Leave"), action: {
-                            self.currentlyInSession = false
-                          }))
-                }
-                VStack {
-                    Spacer()
-                    Button(action: { self.userVM.isAdmin ? (self.showAlert = true) : (self.leaveSession(username: self.userVM.username)) }) {
-                        Text(self.userVM.isAdmin ? "Delete Session" : "Leave Session")
-                            .modifier(ButtonHeavyModifier(isDisabled: false, backgroundColor: Color.red, foregroundColor: Color.white))
-                        
-                    }.alert(isPresented: self.$showAlert) {
-                        Alert(title: Text("Delete Session"),
-                              message: Text("By deleting the current session all members will be kicked."),
-                              primaryButton: .destructive(Text("Delete"), action: {
-                                self.playerStateVM.playerPause()
-                                self.deleteSession(username: self.userVM.username)
-                              }),
-                              secondaryButton: .cancel(Text("Cancel"), action: {
-                                self.showAlert = false
-                              }))
-                    }.padding(.bottom)
-                    
-                }
+            
+            VStack(spacing: 0) {
+                self.topBar.padding()
                 
-            }.sheet(isPresented: self.$showShareSheet) {
+                qrCode
+                
+                Text("Let your friends scan the QR code \nor share the Session-Link to let them join.").font(.footnote).multilineTextAlignment(.center).padding(.bottom)
+                
+                shareLinkButton
+                
+                membersList
+                
+                Spacer()
+                
+                leaveButton
+            }
+            .sheet(isPresented: self.$showShareSheet) {
                 ActivityViewController(activityItems: ["encoreApp://\(self.userVM.sessionID)"] as [Any], applicationActivities: nil)
             }.onAppear{
                 self.getMembers(username: self.userVM.username)
             }
             
             if self.showPopupQRCode {
-                GeometryReader { _ in
-                    VStack(spacing: 0) {
-                        PopupQRCodeView(userVM: self.userVM, showPopupQRCode: self.$showPopupQRCode)
-                    }.frame(width: geo.size.width,
-                            height: geo.size.height,
-                            alignment: .center)
-                    
-                }.background(
-                    Color.black.opacity(0.5)
-                        .edgesIgnoringSafeArea(.all)
-                        .onTapGesture {
-                            self.showPopupQRCode.toggle()
-                        }
-                )
+                popupQRView
             }
         }
     }
     
     var topBar: some View {
-             ZStack {
-                 RoundedRectangle(cornerRadius: 6)
-                     .fill(Color.secondary)
-                     .frame(width: 60, height: 4)
-             }
-         }
+        ZStack {
+            RoundedRectangle(cornerRadius: 6)
+                .fill(Color.secondary)
+                .frame(width: 60, height: 4)
+        }
+    }
+    
+    var qrCode: some View {
+        Button(action: {
+            withAnimation { self.showPopupQRCode.toggle() }
+        }) {
+            QRCodeView(url: "encoreApp://\(self.userVM.sessionID)", size: 150).padding(10)
+        }.buttonStyle(PlainButtonStyle())
+        .alert(isPresented: self.$showSessionExpiredAlert) {
+            Alert(title: Text("Session expired"),
+                  message: Text("The Host has ended the Session."),
+                  dismissButton: .destructive(Text("Leave"), action: {
+                    self.currentlyInSession = false
+                  }))
+        }
+    }
+    
+    var shareLinkButton: some View {
+        Button(action: { self.showShareSheet.toggle() }) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 15).frame(maxWidth: .infinity, maxHeight: 50).foregroundColor(self.colorScheme == .dark ? Color("darkgray") : Color("lightgray"))
+                HStack {
+                    Text("Share Session-Link")
+                        .foregroundColor(self.colorScheme == .dark ? Color.white : Color.black)
+                        .font(.system(size: 15))
+                        .padding(.leading)
+                    Spacer()
+                    Image(systemName: "square.and.arrow.up")
+                        .foregroundColor(self.colorScheme == .dark ? Color.white : Color.black)
+                        .font(.system(size: 20))
+                        .padding(.trailing)
+                }
+            }.padding(.horizontal, 25)
+        }
+    }
+    
+    var membersList: some View {
+        ScrollView {
+            VStack {
+                Spacer().frame(height: 10)
+                ForEach(self.userListVM.members, id: \.self) { member in
+                    HStack {
+                        if member.is_admin {
+                            Text("\(member.username)").bold()
+                        } else {
+                            Text("\(member.username)")
+                        }
+                        Spacer()
+                        if member.is_admin {
+                            Image(systemName: "music.house")
+                        }
+                        else if member.username == self.userVM.username {
+                            Text("You").bold()
+                        }
+                    }
+                    Divider()
+                }
+                Spacer().frame(height: 10)
+            }.padding(.horizontal, 30)
+        }
+    }
+    
+    var leaveButton: some View {
+        Button(action: { self.userVM.isAdmin ? (self.showAlert = true) : (self.leaveSession(username: self.userVM.username)) }) {
+            Text(self.userVM.isAdmin ? "Delete Session" : "Leave Session")
+                .modifier(ButtonHeavyModifier(isDisabled: false, backgroundColor: Color.red, foregroundColor: Color.white))
+        }.padding(.bottom)
+        .alert(isPresented: self.$showAlert) {
+            Alert(title: Text("Delete Session"),
+                  message: Text("By deleting the current session all members will be kicked."),
+                  primaryButton: .destructive(Text("Delete"), action: {
+                    self.playerStateVM.playerPause()
+                    self.deleteSession(username: self.userVM.username)
+                  }),
+                  secondaryButton: .cancel(Text("Cancel"), action: {
+                    self.showAlert = false
+                  }))
+        }
+    }
+    
+    var popupQRView: some View {
+        GeometryReader { geo in
+            VStack(spacing: 0) {
+                PopupQRCodeView(userVM: self.userVM, showPopupQRCode: self.$showPopupQRCode)
+            }.frame(width: geo.size.width,
+                    height: geo.size.height,
+                    alignment: .center)
+            
+        }.background(
+            Color.black.opacity(0.5)
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture {
+                    self.showPopupQRCode.toggle()
+                }
+        )
+    }
     
     func getMembers(username: String) {
         
@@ -268,7 +280,7 @@ struct MenuView: View {
             
             // Convert HTTP Response Data to a String
             if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
+                print("Response data string leave:\n \(dataString)")
             }
             self.currentlyInSession = false
         }
